13dbb52964d5712a5036c7220efc8f16d516d59b
AymericGestin <aymeric.gestin@grenoble-inp.org>
2024-07-18 14:48:36 +0200

    change

--- Code_ecriture_pddl/main.py
+++ Code_ecriture_pddl/main.py
@@ -1,4 +1,4 @@
-from code_writing_pddl_simple import *
+from code_ecriture import *
 import os
 #ecriture_problem("C:/Users/Aymeric/Documents/GitHub/Automated-AI-distribution-network/Code_ecriture_pddl/point_Reseau_16.csv","C:/Users/Aymeric/Documents/GitHub/Automated-AI-distribution-network/Code_ecriture_pddl/RI_Reseau_16.csv","C:/Users/Aymeric/Documents/GitHub/Automated-AI-distribution-network/Code_ecriture_pddl/TR_Reseau_16.csv","C:/Users/Aymeric/Documents/GitHub/Automated-AI-distribution-network/Code_ecriture_pddl/problem_16_V2.pddl")
 os.system('cmd /k "docker run -it -v C:\Users\Aymeric\Documents\GitHub\Automated-AI-distribution-network:/root/projects/benchmarks lapkt/lapkt-public"')
